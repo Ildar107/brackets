97f40b476c4d35dc8b16579930c6f9b3065a643f
ildar107 <ildar107wk@gmail.com>
2019-09-23 17:23:26 +0300

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var bracketsDict = new Map(bracketsConfig);
+  var openBrackets = [];
+  var closeResultString = "";
+  var closeInputString = "";
+  for(var i = 0; i < str.length; i++)
+  {  if(bracketsDict.has(str[i]) && bracketsDict.get(str[i]) !== openBrackets[openBrackets.length - 1])
+    {
+      openBrackets.push(str[i]);
+    }
+    else
+    {
+      if(openBrackets.length === 0)
+        return false;
+      closeResultString += bracketsDict.get(openBrackets.pop());
+      closeInputString += str[i];
+    }
+  }
+  return closeInputString === closeResultString && openBrackets.length === 0;
 }
